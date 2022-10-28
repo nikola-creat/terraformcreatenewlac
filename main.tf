6e0750ad4cadda8b96b76cd2503b858bb27395e3
secretstaraddress <114584373+secretstaraddress@users.noreply.github.com>
2022-10-28 08:38:40 +0500

    /keyvalue

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
 terraform {
-  required_version = ">= 1.0.0"
+
+  required_version = ">= 1.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.26"
+      version = ">= 2.59.0"
     }
+    azuread = {
+      source  = "hashicorp/azurerm"
+      version = ">= 1.5.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "David_Terraform_Backend"
+    storage_account_name = "kuda42terraform"
+    container_name       = "terraform-remote-state"
+    key                  = "secrets.tfstate"
   }
 }
 
@@ -12,25 +25,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "tf_backend" {
-  name = "Terraform_Backend"
-  location = "Switzerland North"
+provider "azuread" {
+  features {}
 }
 
-resource "azurerm_storage_account" "remote_state" {
-  # https://docs.microsoft.com/en-us/azure/storage/common/storage-account-overview#storage-account-endpoints
-  # "Storage account names must be between 3 and 24 characters in length and
-  # may contain numbers and lowercase letters only."
-  # "Your storage account name must be unique within Azure. No two storage 
-  # accounts can have the same name."
-  name = "uniquelowercasename42"
-  resource_group_name = azurerm_resource_group.tf_backend.name
-  location = azurerm_resource_group.tf_backend.location
-  account_tier = "Standard"
-  account_replication_type = "GRS"
+data "azurerm_resource_group" "root" {
+  name = var.resource_group_name
 }
 
-resource "azurerm_storage_container" "remote_state" {
-  name = "terraform-remote-state"
-  storage_account_name = azurerm_storage_account.remote_state.name
-}
+data "azurerm_client_config" "self" {}
