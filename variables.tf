5839b2bf7cda15e089d7d1db2b6e61ee38218062
secretstaraddress <114584373+secretstaraddress@users.noreply.github.com>
2022-10-31 13:58:15 +0500

    /src_3

--- variables.tf
+++ variables.tf
@@ -1,5 +1,29 @@
-variable "resource_group_name" {
+variable "env_tag" {
   type        = string
-  description = "In which Resource Group do you want to build the infrastructure?"
-  default     = "Data_Lake_Distributed_ETL"
+  description = "A tag that describes the environment: dev, test or prod."
+  default     = "dev"
+}
+
+variable "adls_file_systems" {
+  description = "Which filesystems / containers should be created inside the ADLS?"
+  type        = list(string)
+  default     = ["1-landing", "2-raw", "3-clean", "4-core", "5-serve"]
+}
+
+# ~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~
+# Databricks Variables
+
+variable "spark_version" {
+  type    = string
+  default = "8.2.x-scala2.12"
+}
+
+variable "driver_node_type_id" {
+  type    = string
+  default = "Standard_F4s"
+}
+
+variable "node_type_id" {
+  type    = string
+  default = "Standard_F4s"
 }
